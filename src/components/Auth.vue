649214fb009ac05d7d75445fe9ab4b2130647717
Rui Leite <ruipimentelleite@gmail.com>
2020-12-06 14:12:44 -0300

    Cria o formulário de autenticação, prevenindo a ação padrão.

--- src/components/Auth.vue
+++ src/components/Auth.vue
@@ -1,9 +1,38 @@
 <template>
-  <b>Auth works!</b>
+  <div class="container">
+    <h1>Login</h1>
+    <div class="auth-container">
+      <form @submit.prevent="submitHandler">
+        <input
+          autoFocus
+          id="mail"
+          type="text"
+          name="email"
+          placeholder="E-mail"
+        />
+        <input
+          id="password"
+          type="password"
+          name="password"
+          placeholder="Senha"
+        />
+        <button
+          type="submit"
+        >
+          Enviar
+        </button>
+      </form>
+    </div>
+  </div>
 </template>
 
 <script>
   export default {
+    methods: {
+      submitHandler() {
+        console.log('Submeteu!');
+      }
+    }
   }
 </script>
 
