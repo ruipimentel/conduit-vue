d45fcb505d7f62a2a2556cde9c7c6c431002056e
Rui Leite <ruipimentelleite@gmail.com>
2020-12-06 09:00:26 -0300

    Cria o componente `Header` em branco.

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,15 @@
 <template>
   <div id="app">
+    <Header />
   </div>
 </template>
 
 <script>
+import Header from './components/Header';
 export default {
   name: 'App',
   components: {
+    Header
   }
 }
 </script>
