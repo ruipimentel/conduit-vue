a0a71ffc5fcad0ad339a4bb6518a59304eb8706f
Rui Leite <ruipimentelleite@gmail.com>
2020-12-06 09:16:10 -0300

    Introduz conteúdo e estilo no cabeçalho.

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,34 @@
 <template>
-  <div>
-    Header works!
-  </div>
+  <nav>
+    <div class="container">
+      <a
+        href="/"
+        id="logo"
+      >
+        conduit
+      </a>
+      <div class="float-right">
+        <a
+          href="/"
+          class="menu"
+        >
+          Home
+        </a>
+        <a
+          href="/login"
+          class="menu"
+        >
+          Login
+        </a>
+        <a
+          href="/register"
+          class="menu"
+        >
+          Cadastrar
+        </a>
+      </div>
+    </div>
+  </nav>
 </template>
 
 <script>
@@ -10,4 +37,19 @@
 </script>
 
 <style scoped>
+  #logo {
+    font-size: 1.25em;
+    color: #66bc66;
+    font-weight: bold;
+    font-family: 'Courier New', Courier, monospace;
+  }
+  .menu {
+    color: #0000004d;
+    font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+    margin-left: 1em;
+    font-size: 0.9em;
+  }
+  .float-right {
+    float: right;
+  }
 </style>
