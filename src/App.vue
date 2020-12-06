109ba934e5e3422908ad0f8167d6994373644239
Rui Leite <ruipimentelleite@gmail.com>
2020-12-06 09:29:51 -0300

    Introduz conteúdo inicial e estilo do componente `Home`.

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <template>
   <div id="app">
     <Header />
+    <Home />
   </div>
 </template>
 
 <script>
 import Header from './components/Header';
+import Home from './components/Home';
 export default {
   name: 'App',
   components: {
+    Home,
     Header
   }
 }
---
+++ src/components/Home.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="center bg-green">
+    <h1 id="logo">conduit</h1>
+    <p id="lead">
+      Um lugar para você compartilhar todo o seu conhecimento!
+    </p>
+  </div>
+</template>
+
+<script>
+  export default {
+  }
+</script>
+
+<style scoped>
+  .bg-green {
+    padding: 2em;
+    color: white;
+    background-color: #5cb85c;
+  }
+  #logo {
+    font-family: 'Courier New', Courier, monospace;
+    font-size: 3em;
+    text-shadow: 0 1px 3px #0000004d;
+  }
+  #lead {
+    font-size: 1.2em;
+    font-family: Arial, Helvetica, sans-serif;
+  }
+</style>
